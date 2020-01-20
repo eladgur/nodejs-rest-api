e726648c04e99aeb153d182a8d552022f2642a59
eladgur <eladg@madlan.co.il>
2020-01-20 21:55:47 +0200

    add redis

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN npm install
 COPY . .
 #Expose port and start application
 EXPOSE 3000
-CMD [ "npm", "run", "dev" ]
+CMD [ "npm", "start" ]
--- api/cache.js
+++ api/cache.js
@@ -1,7 +1,9 @@
 let mongoose = require('mongoose'),
-    Post = mongoose.model('Posts');
+    Post = mongoose.model('Posts'),
+    redisClient = require('../redis-client');
 
 let topPosts = null
+let redisKey = 'redisKey'
 
 function queryTopPosts() {
     return Post
@@ -14,12 +16,23 @@ function queryTopPosts() {
 }
 
 async function update() {
-    topPosts = await queryTopPosts();
+    let topPosts = await queryTopPosts();
+    await redisClient.setAsync(redisKey, JSON.stringify(topPosts));
+
+    return topPosts;
 }
 
 async function getTopPosts() {
-    if (topPosts === null) await update();
-    return topPosts;
+    let rawData = await redisClient.getAsync(redisKey);
+    let res;
+
+    if (rawData === null) {
+        res = await update();
+    } else {
+        res = JSON.parse(rawData);
+    }
+
+    return res;
 }
 
 module.exports = {
